f091694aeadb49120404f78023f28cef3f20c17a
stepin105439 <69083026+stepin105439@users.noreply.github.com>
2020-10-07 21:43:31 +0530

    Add files via upload

---
+++ inc/cloth_store.h
@@ -0,0 +1,22 @@
+#ifndef __CLOTH_STORE_H__
+	#define __CLOTH_STORE_H__
+	#include<stdio.h>
+    #include<conio.h>
+    #include<string.h>
+    #include<ctype.h>
+    #include<stdlib.h>
+    #include<windows.h>
+    #include<time.h>
+
+	void gotoxy(short x, short y);
+
+	void setcolor(int ForgC);
+
+
+	void add_item();
+	void delete_product();
+	void edit_item();
+	void read_item();
+	void search_item();
+	void total_bill();
+	#endif
